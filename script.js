8d0e6591a0755de4476d590573f4ab43764bfd09
ratansingh98 <ratan.singh98@rediffmail.com>
2020-05-24 03:24:33 +0530

    removed unnecessary files

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const video = document.getElementById('video')
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models'),
   faceapi.nets.ageGenderNet.loadFromUri('/models')
 ]).then(startVideo)
